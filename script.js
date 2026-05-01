afdeb0b9c0128d6d564da03dcd4732d07112af5d
Vincent Iyegbuye <vincentiyegbuye@gmail.com>
2026-05-01 17:30:06 -0500

    Implement core math and operate functions
    Add pure functions for addition, subtraction, multiplication, and
    division. Introduce the operate() function to route an operator
    string and two numbers to the correct math function. Tested via
    browser console.

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 const themeToggle = document.getElementById('theme-toggle');
-        const calculator = document.getElementById('main-calculator');
+const calculator = document.getElementById('main-calculator');
 
-        themeToggle.addEventListener('change', () => {
-            if (themeToggle.checked) {
-                // Switch to Dark Mode
-                calculator.classList.remove('calc-light');
-                calculator.classList.add('calc-dark');
-            } else {
-                // Switch to Light Mode
-                calculator.classList.remove('calc-dark');
-                calculator.classList.add('calc-light');
-            }
-        });
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        // Switch to Dark Mode
+        calculator.classList.remove('calc-light');
+        calculator.classList.add('calc-dark');
+    } else {
+        // Switch to Light Mode
+        calculator.classList.remove('calc-dark');
+        calculator.classList.add('calc-light');
+    }
+});
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "UNDEFINED"
+    }
+    return a / b;
+}
+
+function operate(operator, a, b) {
+
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+
+        case '-':
+            return subtract(a, b);
+
+        case '*':
+            return multiply(a, b)
+
+        case '÷':
+            return divide(a, b);
+
+        default:
+            return null;
+    }
+}
